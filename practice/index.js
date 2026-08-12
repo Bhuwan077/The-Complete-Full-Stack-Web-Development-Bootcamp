df2843d7a5618a891f39d44cdd6cbb59fba821ab
Bhuwan Adhikari <mebhuwan132@gmail.com>
2026-08-13 00:10:59 +0545

    completed the drumkit challenge

--- practice/index.js
+++ practice/index.js
@@ -3,4 +3,7 @@ function BellBoy(name, age, hasWorkPermit, languages) {
   this.age = age;
   this.hasWorkPermit = hasWorkPermit;
   this.languages = languages;
+  this.clean = function() {
+    alert("Cleaning in progress...");
+  }
 }
