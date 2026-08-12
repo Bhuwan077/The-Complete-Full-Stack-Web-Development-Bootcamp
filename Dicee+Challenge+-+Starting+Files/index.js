f7acb1d61f6910cb7db08b371233b6e57626ebca
Bhuwan Adhikari <mebhuwan132@gmail.com>
2026-08-12 21:23:45 +0545

    completed the dice challenge

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -3,10 +3,10 @@ document.querySelector(".img1").setAttribute("src","./images/dice"+randomnumber1
 var randomnumber2=Math.floor(Math.random()*6)+1;
 document.querySelector(".img2").setAttribute("src","./images/dice"+randomnumber2+".png");
 if(randomnumber1>randomnumber2){
-    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
 }
 else if(randomnumber2>randomnumber1){
-    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
 }
 else{
     document.querySelector("h1").innerHTML="It's a Draw!";
