3789b5e7a4586ea881308aa313383ab6b9cd6104
Bhuwan Adhikari <mebhuwan132@gmail.com>
2026-08-14 02:54:49 +0545

    Practising

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// var generateName = require("sillyname");
+// import generateName from 'sillyname';
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import {randomSuperhero} from 'superheroes';
+var superheroes = randomSuperhero();
+console.log(`I am ${superheroes}!`);
